b5f7e526af59ab19068c0cd13458a9aba79ece77
giyun_tf2 <github-email>
2022-05-30 19:38:02 +0900

    N311

---
+++ src/Part_4.py
@@ -0,0 +1,22 @@
+"""
+Part 4
+
+Assignment Requirement
+
+1) 가상환경 목록을 조회하고, 출력 결과를 bash 쉘 리다이렉션 기능을 통해 result.txt 에 저장합니다.
+
+2) 로컬과 원격에 있는 모든 git branch 를 조회하고, 
+   출력 결과를 bash 쉘 리다이렉션 기능을 통해 result2.txt 에 저장합니다.
+
+3) `git-branch` 를 `codestates/simple-git-flow` `main` branch 에 PR을 수행한 후
+   로컬과 원격에 있는 모든 git branch 를 조회하고, 
+   출력 결과를 bash 쉘 리다이렉션 기능을 통해 result3.txt 에 저장합니다.
+
+아래에 코드에 요구사항에 맞는 Bash 쉘 기반 CLI 명령어를 작성합니다.
+요구사항을 잘 보고, 상황에 맞게 정확히 작성해 주세요.
+또한, 로컬에서 해당 명령어를 실행하면서 따라와 주세요.
+
+** 대소문자를 정확히 구분해서 작성해 주세요. **
+"""
+
+VIRTUAL_ENV_LIST = "콘다 가상환경의 목록을 조회하고, 출력된 결과를 result.txt에 저장하는 CLI 명령어를 작성합니다."
